858a303e8f0015e274f76879812eb7cb5f47e752
zxb <zhangxiaobo@wenyu.com>
2021-03-02 00:16:29 +0800

    格式化配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: wenyu_zxb
+ * @Date: 2021-03-01 11:53:06
+ * @LastEditTime: 2021-03-01 11:53:16
+ * @LastEditors: wenyu_zxb
+ * @Description:
+ * @FilePath: /TypeScript/.prettierrc.js
+ */
+module.exports = {
+  semi: false,
+  arrowParens: 'avoid',
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  printWidth: 120,
+  trailingComma: 'none'
+}
